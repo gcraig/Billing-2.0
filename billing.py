a1a86c13ea20ad0d356b65dcd1a451f564294bc6
gcraig <georgeacraig@gmail.com>
2014-05-03 08:35:02 -0500

    added simple template processor

--- billing.py
+++ billing.py
@@ -1,19 +1,37 @@
 import sys
+import string
 import couchdb
 
 from couchdb.mapping import Document, TextField, IntegerField, DateTimeField
 
-class Invoice(Document):
+class Invoice( Document ):
   invoice_number = TextField()
 
 couch = couchdb.Server()
 db = couch['billing']
 
-for id in db:
-  print id
-  invoice = Invoice.load(db, id)
-  invoice.invoice_number = str(int(invoice.invoice_number) + 1)
-  print invoice
-  invoice.store(db)
+def read_template( invoice ):
+  filein = open( 'invoice.json.tpl' )
+  tpl = string.Template( filein.read() )
+  data =  { 'invoice_number' : invoice.invoice_number }
+  result = tpl.substitute( data )
+  return result
+
+def read_invoice( db ):
+  id = 'a9f88ba1a38ec88771cb5974db0012a3'
+  invoice = Invoice.load( db, id )
+  invoice.invoice_number = str( int( invoice.invoice_number ) + 1 )
+  data = read_template( invoice )
+  print data
+  invoice.store( db )
+
+read_invoice( db )
+
+#for id in db:
+#  print id
+#  invoice = Invoice.load(db, id)
+#  invoice.invoice_number = str(int(invoice.invoice_number) + 1)
+#  print invoice
+#  invoice.store(db)
 
 # invoice.json.tmpl
